c204d26f068a20e098de0dd09e549982311f3eeb
anandg112 <anandg112@gmail.com>
2017-07-09 01:25:54 -0400

    made changes to server

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var express = require('express');
-
 var app = express();
+
+var port = process.env.PORT || 8080;
 
 app.use('/', express.static(__dirname + '/public'));
 
 
-app.listen(3000, () => { console.log('listening on port 3000')});
+app.listen(port, () => { console.log(`listening on port ${port}`)});
